f17aaa4c4ec1b460cdd0629ef6a8fb490921ed6a
Seiji Sawayanagi <mostlyfine@gmail.com>
2010-11-17 03:07:03 +0900

    first commit

---
+++ config.ru
@@ -0,0 +1,7 @@
+require 'rubygems'
+require 'sinatra'
+require 'haml'
+require 'redcloth'
+require 'sinatra_more/markup_plugin'
+
+run Sinatra::Application
---
+++ siki.rb
@@ -0,0 +1,53 @@
+require 'rubygems'
+require 'sinatra'
+require 'haml'
+require 'redcloth'
+require 'sinatra_more/markup_plugin'
+
+Sinatra::Base.register SinatraMore::MarkupPlugin
+
+before do
+  content_type 'text/html', :charset => 'utf-8'
+end
+
+get '/' do
+  @page = params[:page] || 'FrontPage'
+  filename = filepath(@page)
+  if File.exist? filename
+    dat = File.open(filename).read
+    @content = RedCloth.new(dat).to_html
+    haml :show
+  else
+    haml :edit
+  end
+end
+
+get '/list' do
+  @files = Dir.glob("#{datadir}/*#{dataext}")
+  haml :list
+end
+
+get '/edit' do
+  @content = File.open(filepath(params[:page])).read rescue nil
+  haml :edit
+end
+
+put '/' do
+  File.open(filepath(params[:page]), 'w') do |file|
+    file.puts params[:body]
+  end
+  redirect "/?page=#{params[:page]}"
+end
+
+private
+def datadir
+  'data'
+end
+
+def dataext
+  '.txt'
+end
+
+def filepath(name)
+  File.join(datadir, name + dataext)
+end
